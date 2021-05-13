14eeba4c6976aecbcde50084fc88dac948a95267
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2021-05-13 15:16:06 +0900

    Update

--- README.md
+++ README.md
@@ -6,10 +6,37 @@ A simple library to cleaning up url and email address.
 [![license](https://img.shields.io/github/license/y-bar/netcleanser.svg)](https://github.com/y-bar/netcleanser/blob/master/LICENSE)
 [![release](https://img.shields.io/github/release/y-bar/netcleanser.svg)](https://github.com/y-bar/netcleanser/releases/latest)
 
-## Documentation
 
-### Installation
+## Install
 
 ```bash
 pip install netcleanser
+```
+
+## How to use
+
+### Email 
+
+```python
+>>> from netcleanser import Email
+>>> email = Email('shinichi.takayanagi@gmail.com')
+>>> email.domain
+'gmail.com'
+>>> email.local_part
+'shinichi.takayanagi'
+>>> email.is_valid()
+True
+>>> email.value
+'shinichi.takayanagi@gmail.com'
+```
+
+This `Email` class is `settable` and `dictable`
+```python
+# As dict
+>>> x = {email: 1}
+>>> x[email]
+1
+# As set
+>>> {Email.build(), email, email, email}
+{Email(value='shinichi.takayanagi@gmail.com)', Email(value='dummy@dummy.com)'}
 ```
--- tests/test_email.py
+++ tests/test_email.py
@@ -1,22 +1,52 @@
 from netcleanser import Email
 
 
-def test_email_1():
-    email = Email("hoge.moge@gmail.com")
-    assert email.local_part == "hoge.moge"
-    assert email.domain == "gmail.com"
-    assert str(email) == email.value
+def test_email():
+    for value in ["hoge.moge@gmail.com", "hoge.moge＠gmail.com"]:
+        email = Email(value)
+        assert email.is_valid() == True
+        assert email.local_part == "hoge.moge"
+        assert email.domain == "gmail.com"
+        assert str(email) == email.value
+        assert Email.build(email.local_part, email.domain) == email
 
-def test_email_2():
-    email = Email(None)
-    assert email.local_part is None
-    assert email.domain is None
-    assert email.value is None
+def test_empty_local_part():
+    for value in ["gmail.com", "@gmail.com"]:
+        email = Email(value)
+        assert email.is_valid() == True
+        assert email.local_part == ""
+        assert email.domain == "gmail.com"
+        assert str(email) == email.value
+        assert Email.build(email.local_part, email.domain) == email
 
-def test_email_3():    
+def test_internationalized_domain():
+    for value in ["あうふへぇ@ほげほげ.com", "あうふへぇ＠ほげほげ.com"]:
+        email = Email(value)
+        assert email.is_valid() == True
+        assert email.local_part == "あうふへぇ"
+        assert email.domain == "ほげほげ.com"
+        assert str(email) == email.value
+        assert Email.build(email.local_part, email.domain) == email
+
+def test_email_is_invalid():
+    for value in ["", "akdsjfkjh", None]:
+        email = Email(value)
+        assert email.is_valid() == False
+        assert email.local_part is None
+        assert email.domain is None
+        assert email.value is None
+
+def test_email_build():    
     email = Email().build(domain="hoge.com")
     assert email.local_part == "dummy"
     assert email.domain == "hoge.com"
 
+    email = Email.build(local_part="hoge")
+    assert email.local_part == "hoge"
+    assert email.domain == "dummy.com"
+
 def test_email_should_be_settable():
-    xs = {Email("hoge@gmail.com"), Email("xxx@yahooo")}
+    {Email("hoge@gmail.com"), Email("xxx@yahooo")}
+
+def test_email_should_be_dictable():
+    {Email("hoge@gmail.com"): 123, Email("xxx@yahooo"): 456}
