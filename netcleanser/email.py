02a458fc4a07ed674afa730ef8e398d6f0a2e69b
Shinichi Takayanagi <shinichi.takayanagi@gmail.com>
2021-05-19 20:09:25 +0900

    Add types

--- netcleanser/email.py
+++ netcleanser/email.py
@@ -33,19 +33,19 @@ class Email:
         self._local_part = None
         self._domain = None
  
-    def __str__(self):
+    def __str__(self) -> str:
         return self._value
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         # The same format with dataclass ;)
         return f"Email(value='{self._value})'"
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> bool:
         if not isinstance(other, Email):
             return False
         return self._value == other._value
     
-    def __hash__(self):
+    def __hash__(self) -> int:
         return hash(self._value)
 
     def is_valid(self) -> bool:
